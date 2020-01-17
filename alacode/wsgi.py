633ed694cab2cf7cc8089833425041266d60da22
mjsteijaert <59570190+mjsteijaert@users.noreply.github.com>
2020-01-17 09:50:04 +0100

    Update wsgi.py

--- alacode/wsgi.py
+++ alacode/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alacode.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "excel_upload.settings")
 
 application = get_wsgi_application()
